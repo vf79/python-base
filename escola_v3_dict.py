4e5316f746a85c1d8ea695648c4e1307a0e33357
Valmir França <vf79@yahoo.com>
2023-03-20 15:24:45 -0300

    Add exemplo dict an upgrade hello.py

---
+++ escola_v3_dict.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Exibe relatório de crianças por atividade.
+
+Imprimir a lista de crianças agrupadas por sala que frequentam cada uma das 
+atividades.
+"""
+__version__ = "0.1.0"
+
+sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
+sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+
+
+aulas = {
+    "Inglês": ["Erik", "Maia", "Joana", "Carlos", "Antonio"],
+    "Música": ["Erik", "Carlos", "Maria"],
+    "Dança": ["Gustavo", "Sofia", "Joana", "Antonio"],
+}
+
+# Listar alunos em cada atividade por sala
+
+for aula, alunos in aulas.items():
+
+    print(f"Alunos da atividade {aula}\n")
+    print("-" * 80)
+
+    atividade_sala1 = []
+    atividade_sala2 = []
+
+    atividade_sala1 = set(sala1) & set(alunos)
+    atividade_sala2 = set(sala2).intersection(alunos)
+
+
+    print("Sala1 ", atividade_sala1)
+    print("Sala2 ", atividade_sala2)
+    print()
+    print("-" * 80)
+    
+    #for aluno in alunos:
+    #    if aluno in sala1:
+    #        atividade_sala1.append(aluno)
+    #    elif aluno in sala2:
+    #        atividade_sala2.append(aluno)
+#
+    #print("Sala1 ", atividade_sala1)
+    #print("Sala2 ", atividade_sala2)
+    #print()
+    #print("-" * 80)
+#
--- hello.py
+++ hello.py
@@ -18,7 +18,7 @@ Execução:
     python3 hello.py
     ou ./hello.py
 """
-__version__="0.1.0"
+__version__="0.1.2"
 __author__="vf79"
 __license__="Unlicense"
 
@@ -26,15 +26,24 @@ import os
 
 current_language = os.getenv("LANG", "en_US")[:5]
 
-msg = "Hello, World!"
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!",
+}
 
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-elif current_language == "es_SP":
-    msg = "Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour, Monde!"
+print (msg[current_language])
+# sets (Hash Table) - O(1) - constante
+# Ordem Complexidade O(n)
 
-print(msg)
+#if current_language == "pt_BR":
+#    msg = "Olá, Mundo!"
+#elif current_language == "it_IT":
+#    msg = "Ciao, Mondo!"
+#elif current_language == "es_SP":
+#    msg = "Hola, Mundo!"
+#elif current_language == "fr_FR":
+#    msg = "Bonjour, Monde!"
+#print(msg)
