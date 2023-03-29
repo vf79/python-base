e2b1ad68a9e54f931c89ea615b38283ee0000d7a
Valmir França <vf79@yahoo.com>
2023-03-29 09:33:37 -0300

    Refatorado hello.py com argumentos e adicionado arquivo infixcalc para fixação de conteúdo

--- hello.py
+++ hello.py
@@ -18,13 +18,42 @@ Execução:
     python3 hello.py
     ou ./hello.py
 """
-__version__="0.1.2"
+__version__="0.1.3"
 __author__="vf79"
 __license__="Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+#print(f"{sys.argv=}")
+
+arguments = {
+    "lang": None,
+    "count": 1,
+}
+
+for arg in sys.argv[1:]:
+    # TODO: Tratar ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option `{key}`")
+        sys.exit
+    arguments[key] = value
+
+current_language = arguments["lang"]
+
+if current_language is None:
+    # TODO: Usar repetição
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language:")
+
+
+current_language = current_language[:5]
+
 
 msg = {
     "en_US": "Hello, World!",
@@ -34,7 +63,7 @@ msg = {
     "fr_FR": "Bonjour, Monde!",
 }
 
-print (msg[current_language])
+print (msg[current_language] * int(arguments["count"]))
 # sets (Hash Table) - O(1) - constante
 # Ordem Complexidade O(n)
 
---
+++ infixcalc.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""Calculadora infix.
+
+
+Funcionamento:
+
+[operacao] [n1] [n2]
+
+Operações:
+
+sum -> +
+sub -> -
+mul -> *
+div -> /
+
+Uso:
+$ infixcalc.py sum 5 2
+7
+
+$ infixcalc.py mul 10 5
+50
+
+$ infixcalc.py
+operacao: sum
+n1: 5
+n2: 4
+9
+"""
+__version__ = "0.1.0"
+
+import sys
+arguments = sys.argv[1:]
+
+# TODO: Exceptions
+if not arguments:
+    operation = input("operação:")
+    n1 = input("n1:")
+    n2 = input("n2:")
+    arguments = [operation, n1, n2]
+elif len(arguments) != 3:
+    print("Número de argumentos inválidos")
+    print("ex: `sum 5 5`")
+    sys.exit(1)
+
+operation, *nums = arguments
+
+valid_operations = ("sum", "sub", "mul", "div")
+if operation not in valid_operations:
+    print("Operação inválida")
+    print(valid_operations)
+    sys.exit(1)
+
+validated_nums = []
+
+for num in nums:
+    # TODO: Repetição while + exceptions
+    if not num.replace(".", "").isdigit():
+        print(f"Número inválido {num}")
+        sys.exit(1)
+    if "." in num:
+        num = float(num)
+    else:
+        num = int(num)
+    validated_nums.append(num)
+
+n1, n2 = validated_nums
+
+# TODO: Usar dict de funcoes
+if operation =="sum":
+    result = n1 + n2
+elif operation =="sub":
+    result = n1 - n2
+elif operation =="mul":
+    result = n1 * n2
+elif operation =="div":
+    result = n1 / n2
+
+print(f"O resultado é {result}")
