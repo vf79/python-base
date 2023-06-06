6c5a7f72c3fc51cd31db1cd015de14df90eedec1
Valmir França <vf79@yahoo.com>
2023-06-06 14:53:40 -0300

    Add more exercises

--- exercicios.py
+++ exercicios.py
@@ -1,4 +1,8 @@
 #!/bin/usr/env python3
+
+import sys
+import logging
+
 """
 Faça um programa que imprime os números pares de 1 a 200
 """
@@ -20,8 +24,7 @@ Senão, se temp vezes 3 for maior ou igual a umidade: ALERTA!!! Perigo de calor 
 ...temp entre 0 e 10: Frio
 ...temp <0: ALERTA: Frio extremo
 """
-import sys
-import logging
+
 log = logging.Logger("alerta")
 
 def alarme_temp_opt():
@@ -98,6 +101,106 @@ def repete_vogal():
     #    print(word)
     print(*words, sep="\n")
 
+"""
+Faça um programa de terminal que exibe ao usuário um listas dos quartos disponíveis 
+para alugar e o preço de cada quarto, esta informação está disponível em um arquivo 
+de texto separado por vírgulas.
+
+`quartos.txt`
+# codigo, nome, preço
+1,Suite Master,500
+2,Quarto Familia,200
+3,Quarto Single,100
+4,Quarto Simples,50
+
+O programa pergunta ao usuário o nome, qual o número do quarto a ser reservado
+e a quantidade de dias e no final exibe o valor estimado a ser pago.
+
+`reservas.txt`
+# cliente, quarto, dias
+Bruno,3,12
+
+Se outro usuário tentar reservar o mesmo quarto o programa deve exibir uma 
+mensagem informando que já está reservado.
+
+"""
+def reserva_quarto():
+    ocupados = {}
+    try:
+        for line in open("reservas.txt"):
+            nome, num_quarto, dias = line.strip().split(",")
+            ocupados[int(num_quarto)] = {
+                "nome": nome,
+                "dias": dias,
+            }
+    except FileNotFoundError:
+        logging.error("Arquivo reservas.txt não existe")
+        sys.exit(1)
+
+    quartos = {}
+    try:
+        for line in open("quartos.txt"):
+            codigo,nome,preco = line.strip().split(",")
+            quartos[int(codigo)] = {
+                "nome": nome,
+                "preco": float(preco), # TODO: Decimal,
+                "disponivel": False if int(codigo) in ocupados else True
+            }
+    except FileNotFoundError:
+        logging.error("Arquivo quartos.txt não existe")
+        sys.exit(1)
+
+
+
+    print("Reserva Hotel Pythonico")
+    print("-" * 80)
+    
+    if len(ocupados) == len(quartos):
+        print("Hotel Lotado")
+        sys.exit(1)
+
+    nome = input("Nome do cliente:").strip()
+
+    print("Lista de quartos disponíveis")
+    for codigo, dados in quartos.items():
+        nome_quarto = dados["nome"]
+        preco = dados["preco"]
+        # disponivel = dados['disponivel'] and "👍" or "⛔"
+        disponivel = "⛔" if not dados["disponivel"] else "👍"
+        # TODO: Substituir casa decimal por virgula
+        print(f"{codigo} - {nome_quarto} - R$ {preco:.2f} - {disponivel}")
+
+    print("-" * 80)
+    try:
+        num_quarto = int(input("Número do quarto: ").strip())
+        if not quartos[num_quarto]["disponivel"]:
+            print(f"O quarto {num_quarto} está ocupado.")
+            sys.exit(1)
+    except ValueError:
+        logging.error("Número inválido, digite apenas digitos.")
+        sys.exit(1)
+    except KeyError:
+        print(f"O quarto {num_quarto} não existe.")
+        sys.exit()
+    
+    try:
+        dias = int(input("quantos dias:").strip())
+    except ValueError:
+        logging.error("Número inválido, digite apenas digitos.")
+        sys.exit(1)
+
+    nome_quarto = quartos[num_quarto]["nome"]
+    preco_quarto = quartos[num_quarto]["preco"]
+    disponivel = quartos[num_quarto]["disponivel"]
+    total = preco_quarto * dias
+
+    #print(f"{nome},{num_quarto},{dias}")
+    #print(",".join([nome, str(num_quarto), str(dias)]))
+    with open("reservas.txt", "a") as file_:
+        file_.write(f"{nome},{num_quarto},{dias}\n")
+    print(f"{nome} voce escolheu o quarto {nome_quarto} e vai custa: R${total:.2f}")
+
 # numeros_pares()
 # alarme_temp_opt()
-repete_vogal()
+# repete_vogal()
+reserva_quarto()
